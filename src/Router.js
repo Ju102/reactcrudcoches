b0e5424218abddfbee0f85dc6064780d8642f85f
Ju102 <jlordonez02@gmail.com>
2025-10-23 09:54:12 +0200

    Add dependencies and initial component structure
    - Added axios, bootstrap, jquery, popper.js, and react-router-dom to package.json and package-lock.json.
    - Created Global.js for API endpoint configuration.
    - Implemented Router.js for routing setup.
    - Added App.js and App.css for main application structure and styling.
    - Included App.test.js for initial testing of the App component.
    - Updated index.js to include Bootstrap and jQuery.

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+class Router extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route />
+                </Routes>
+            </BrowserRouter>
+        )
+    }
+}
+
+export default Router
